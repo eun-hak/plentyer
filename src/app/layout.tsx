0965b6cdf2f02db84ba8663908d6286ed897fdd0
eun-hak <akfmzh1207@naver.com>
2026-01-28 08:57:45 +0900

    feat: test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,8 @@ export const metadata = {
       "naver-site-verification": "094139038ce6770db27aa5907e4d187026f34334",
     },
   },
+
+  //
   icons: {
     icon: [
       { url: "/favicon.svg", type: "image/svg+xml" },
